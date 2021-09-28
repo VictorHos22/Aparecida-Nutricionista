832b9222e1fd51bb4faf7fe3cf56f3818b4df12f
Victor Oliveira <victorhenriqueosilva@gmail.com>
2021-09-28 08:15:13 -0300

    projeto completo

---
+++ js/calcula-imc.js
@@ -0,0 +1,53 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent="Aparecida Nutricionista"
+
+var pacientes=document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+	var paciente = pacientes[i]
+	var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+	var tdImc = paciente.querySelector(".info-imc");
+	
+	var peso = tdPeso.textContent;
+	var altura = tdAltura.textContent;
+
+	var pesoEhValido = validaPeso(peso);
+	var alturaEhValida = validaAltura(altura);
+
+	if(!pesoEhValido){
+	console.log("Peso Inválido");
+	tdImc.textContent = "Peso Inválido!";
+	pesoEhValido = false;
+	paciente.classList.add("paciente-invalido");
+	}
+	if(!alturaEhValida){
+		console.log("Altura Inválida!");
+		tdImc.textContent = "Altura Inválida";
+		alturaEhValida = false;
+		paciente.classList.add("paciente-invalido");
+	}
+	if(pesoEhValido && alturaEhValida){
+	var imc = calculaImc(peso,altura);
+	tdImc.textContent = imc;
+	} 
+}
+function calculaImc(peso,altura){
+	var imc = 0;
+	imc = peso/(altura*altura);
+	return imc.toFixed(2);
+}
+function validaPeso(peso){
+	if(peso >0 && peso <1000){
+		return true;
+	}else{
+		return false;
+	}
+}
+function validaAltura(altura){
+	if(altura > 0 && altura < 3.00){
+		return true
+	}else{
+		return false
+	}
+}
